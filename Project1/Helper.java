c9cf33d94ee426a55c34b61a2628e07d5ff3338a
Zachary Sweet <zsweet@ucsc.edu>
2019-11-21 11:05:17 +0100

    Implemented Gaussian elimination and the outline of the factor function

--- Project1/Helper.java
+++ Project1/Helper.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
-
 public class Helper {
     private static int fLength = 1024 ;
     private static int[] factorBase = generateFactorBase(fLength);
     private static int smoothness = factorBase[fLength-1] + 1 ;
     private static int rLength = fLength + 10;
+    private static int[] rValues = new int[rLength] ;
 
     private static int[] generateFactorBase(int size) {
         int n = 0, i, j, k;
         int[] primes = new int[size];
         for (i = 2; n < size; i++) {
-            k = Math.sqrt(i) + 1 ;
+            k = (int) Math.floor(Math.sqrt(i) + 1) ;
             for (j = 0; j < n && primes[j]<=k; j++) {
                 if ((i % primes[j])==0) {
                     j = 0;
@@ -44,8 +44,8 @@ public class Helper {
     
     private static ArrayList<Integer> primeFactors(BigInteger x){
         ArrayList<Integer> primeFactors = new ArrayList<Integer>();
-        for(BigInteger i=BigInteger.valueOf(2); i.compareTo(x) == -1; i++){
-            while(x.mod(i)==0){
+        for(BigInteger i=BigInteger.valueOf(2); i.compareTo(x) == -1; i = i.add(BigInteger.ONE)){
+            while(x.mod(i).equals(BigInteger.ZERO)){
                 primeFactors.add(i.intValue());
             }
         }if(x.intValue()>2){
@@ -57,8 +57,8 @@ public class Helper {
     private static LinkedHashMap<Integer,Integer> primeMap(ArrayList<Integer> primeFactors){
         LinkedHashMap<Integer,Integer> primeMap = new LinkedHashMap<Integer,Integer>();
         for(int i=0; i<primeFactors.size(); i++){
-            int count = primeMap.containsKey(primeFactors.get(i)) ? primeMap.get(word) : 0;
-            primeMap.put(word, count + 1);
+            int count = primeMap.containsKey(primeFactors.get(i)) ? primeMap.get(i) : 0;
+            primeMap.put(primeFactors.get(i), count + 1);
         }
         return primeMap;
     }
@@ -76,15 +76,47 @@ public class Helper {
                 return false;  
         return true; 
     }
+    
+    // Call this function from main() with BigInteger as a string
+    public static int[][] factorFromString throws IOException (String n) {
+        BigInteger x = new BigInteger(n) ;
+        int[][] exponents = getExponents(x) ;
+        int[][] possibles = eliminate(exponents) ;
+        
+        for (int i = 0; i < possibles.length; i++) {
+            BigInteger combinedR = BigInteger.ONE ;
+            int[] combined_exp = new int[fLength] ;
+            for (int j = 0; j < rLength; j++) {
+                if (possibles[i][j]) {
+                    combinedR = combinedR.multiply(BigInteger.valueOf(rValues[j])) ;
+                    for (int k = 0; k < fLength; k++) {
+                        combined_exp[k] = exponents[k][j] ;
+                    }
+                }
+            }
+            
+            BigInteger combinedFactors = BigInteger.ONE;
+            for (int j = 0; j < fLength; j++) {
+                combinedFactors = combinedFactors.multiply(BigInteger.valueOf(factorBase[j]).pow(combined_exp[j])) ;
+            }
+            
+            // test(int r, int factors) is not yet implemented
+            // Should just do the gcd of the two numbers
+            int result = test(combinedR, combinedFactors) ;
+            if (result != 1) {
+                return result ;
+            }
+        }
+    }
 
-    public static factor(BigInteger n) {
+    private static int[][] getExponents(BigInteger n) {
         //matrix holds values of exponents
         //gaussian elimination then converts matrix to binary
         int[][] matrix = new int[rLength][fLength];
         int[] tempRow = new int[fLength];
         int[] tempRowBinary = new int[fLength];
         int currentRowIndex = 0;
-        int j,k = 1;
+        int j = 1,k = 1;
         BigInteger r;
         ArrayList<Integer> pFactors = new ArrayList<Integer>();
         LinkedHashMap<Integer,Integer> pMap = new LinkedHashMap<Integer,Integer>();
@@ -92,7 +124,7 @@ public class Helper {
         //increment j when want to test a new r, until == sqrt(n), then reset j and increment k
         //increment k when found an r that works
         while(currentRowIndex < rLength){
-            r = floor(squareRoot(n.multiply(BigInteger.valueOf(k)))) + j;
+            r = squareRoot(n.multiply(BigInteger.valueOf(k))).add(BigInteger.valueOf(j));
             pFactors = primeFactors((r.pow(2)).mod(n));
 
             
@@ -147,11 +179,52 @@ public class Helper {
             for(int i=0;i<fLength;i++){
                 matrix[currentRowIndex][i] = tempRow[i];
             }
+            rValues[currentRowIndex] = r ;
             k++;
             currentRowIndex++;
             pMap.clear();
             pFactors.clear();
         }
-
+        return matrix ;
+    }
+    
+    private static int[][] eliminate(int[][] exponents) throws IOException {
+        // Write matrix to file
+        int m = 12 ;
+        int n = 10 ;
+        String firstLine = "" + m + " " + n ;
+        String matrix = firstLine + "\n";
+        for (int i = 0; i < m; i++) {
+            matrix = matrix + "" + exponents[i][0] ;
+            for (int j = 1; j < n; j++) {
+                matrix = matrix + " " + exponents[i][j] ;
+            }
+            matrix = matrix + "\n" ;
+        }
+        String test = "test" ;
+        FileWriter fileWriter = new FileWriter("gauss.in") ;
+        fileWriter.write(matrix) ;
+        fileWriter.close() ;
+        
+        // Run GaussBin.exe to get the binary matrix
+        Process gaussBin = new ProcessBuilder("GaussBin.exe", "gauss.in", "gauss.out").start() ;
+        
+        // Output matrix from gauss.out
+        BufferedReader reader = new BufferedReader(new FileReader("gauss.out")) ;
+        m = Integer.parseInt(reader.readLine()) ; // get number of rows for output
+        int[][] result = new int[m][n] ;
+        String line = reader.readLine() ;
+        for (int i = 0; i < m; i++) {
+            String[] temp = line.split(" ") ;
+            for (int j = 0; j < n; j++) {
+                result[i][j] = Integer.parseInt(temp[j]) ;
+            }
+        }
+        reader.close() ;
+        return result ;
+    }
+    
+    public static int[][] eliminateTester(int[][] exponents) throws IOException {
+        return eliminate(exponents) ;
     }
 }
