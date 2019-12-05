1b595c8819a9451258f3c2545ab1ae550b3cf9af
alexiscole99 <alexiscole@umail.ucsb.edu>
2019-12-05 14:54:08 +0100

    initial Project2

--- Project2/Project2.java
+++ Project2/Project2.java
@@ -1,6 +1,6 @@
 public class Solution{
     public static void main(String[] args){
         //construct 2 db sequences
-        //use a function to map to Z_10 i.e. 5x+y
+        //use a function to map to Z_10 i.e. 5x+y or x+2y or 5x+2y(mod10)
     }
 }
