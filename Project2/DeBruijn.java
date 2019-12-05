458dcc18db36f788bbdc0fb2fb1edd540d21abfe
alexiscole99 <alexiscole@umail.ucsb.edu>
2019-12-05 11:55:07 +0100

    initial Project2

---
+++ Project2/DeBruijn.java
@@ -0,0 +1,5 @@
+public class DeBruijn{
+    //C(D): D^4+D+1 gives x values
+    //C(D): 2D^4+2D^3+2D^2+2D+1 gives y values
+
+}
---
+++ Project2/Project2.java
@@ -0,0 +1,6 @@
+public class Solution{
+    public static void main(String[] args){
+        //construct 2 db sequences
+        //use a function to relate them i.e. 5x+y
+    }
+}
