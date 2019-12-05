5414209f061239678d1eca32ee972648848f8cae
alexiscole99 <alexiscole@umail.ucsb.edu>
2019-12-05 11:57:02 +0100

    initial Project2

--- Project2/Project2.java
+++ Project2/Project2.java
@@ -1,6 +1,6 @@
 public class Solution{
     public static void main(String[] args){
         //construct 2 db sequences
-        //use a function to relate them i.e. 5x+y
+        //use a function to map to Z_10 i.e. 5x+y
     }
 }
