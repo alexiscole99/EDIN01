754710e960f6e4005f93cd04e909ba57bb89cb83
alexiscole99 <alexiscole@umail.ucsb.edu>
2019-12-08 22:31:25 +0100

    Project3 initial commit

--- Project3/CorrAttack.java
+++ Project3/CorrAttack.java
@@ -7,19 +7,48 @@ public class CorrAttack{
     private int cd2 = new int[] {0,1,0,1,0,1,1,0,0,1,1,0,1,0,1};
     private int L3 = 17;
     private int cd3 = new int[] {0,1,0,1,1,0,0,1,0,1,0,0,1,0,0,1,1};
-    private ArrayList<Integer> givenSeq;
+    private String givenSeq;
     private int seqLength;
-    private ArrayList<ArrayList<Integer>> K;
+    private ArrayList<String> K1;
+    private ArrayList<String> K2;
+    private ArrayList<String> K3;
 
     public CorrAttack(String sequence){
-        givenSeq  = new ArrayList<Integer>();
-        K = new ArrayList<ArrayList<Integer>>();
-        for(int i=0;i<sequence.length();i++){
-            givenSeq.add(Character.getNumericValue(sequence.charAt(i)));
-        }
+        givenSeq  = new ArrayList<String>();
+        K1 = new ArrayList<String>();
+        K2 = new ArrayList<String>();
+        K3 = new ArrayList<String>();
+        givenSeq = sequence;
         seqLength = givenSeq.size();
     }
 
+    public void generatePossibleKeys(){
+        String s;
+        for(int i = 0; i<(int)Math.pow(i,13); i++){
+            s = Integer.toBinaryString(i);
+            K1.add(s);
+        }
+        for(int i = 0; i<(int)Math.pow(i,15); i++){
+            s = Integer.toBinaryString(i);
+            K2.add(s);
+        }
+        for(int i = 0; i<(int)Math.pow(i,17); i++){
+            s = Integer.toBinaryString(i);
+            K3.add(s);
+        }
+
+    }
+
+    public int hammingDistance(String x, String y){
+        int dist = 0;
+        for(int i=0;i<x.size();i++){
+            if(x.charAt(i)!=y.charAt(i)){
+                dist++;
+            }
+        }
+        return dist;
+    }
+    
     /*
     1. generate all possible sequences of LFSRs
     2. generate remaining digits for each possible sequence
